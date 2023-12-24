63fcba719024ebddc44dabf7d895354aa83979ee
Imran Sarwar <imranbutt.java@gmail.com>
2023-12-24 20:27:21 +0000

    feat(stage-2) review final

--- sql/reviewboard.sql
+++ sql/reviewboard.sql
@@ -12,3 +12,17 @@ CREATE TABLE IF NOT EXISTS companies (
     image TEXT,
     tags TEXT
 );
+
+CREATE TABLE IF NOT EXISTS reviews (
+  id  BIGSERIAL PRIMARY KEY,
+  company_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL,
+  management INT,
+  culture INT,
+  salary INT,
+  benefits INT,
+  would_recommend INT,
+  review TEXT,
+  created TIMESTAMP NOT NULL DEFAULT now(),
+  updated TIMESTAMP NOT NULL DEFAULT now()
+);
